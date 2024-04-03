673582028340af812664668abbd8a37e10c49d98
juleshb <habaruremajules@gmail.com>
2024-04-03 16:18:29 +0530

    change slide

--- src/components/container/Slide.jsx
+++ src/components/container/Slide.jsx
@@ -3,7 +3,7 @@ import { Icon } from '@iconify/react';
 
 const Slide = ({ image, text }) => {
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center ">
       <div className="max-w-lg mx-auto">
         <div className="border-[3px] border-solid border-Teal rounded-lg">
           <img src={image} alt="Slide" className="transition-all duration-500 ease-in-out transform scale-100 hover:scale-105 p-4" />
