2360ed1425a895f49064c536a205feac60d23f34
juleshb <habaruremajules@gmail.com>
2024-04-03 16:12:37 +0530

    change slide

--- src/components/container/Slide.jsx
+++ src/components/container/Slide.jsx
@@ -5,10 +5,10 @@ const Slide = ({ image, text }) => {
   return (
     <div className="flex items-center justify-center h-screen">
       <div className="max-w-lg mx-auto">
-        <div className="overflow-hidden rounded-lg shadow-lg">
-          <img src={image} alt="Slide" className="transition-all duration-500 ease-in-out transform scale-100 hover:scale-105" style={{ width: '600px', height: '400px' }} />
+        <div className="border-[3px] border-solid border-Teal rounded-lg">
+          <img src={image} alt="Slide" className="transition-all duration-500 ease-in-out transform scale-100 hover:scale-105 p-4" />
         </div>
-        <p className="text-xl text-center transition-all duration-500 ease-in-out transform opacity-100 hover:opacity-50"><Icon icon="svg-spinners:3-dots-fade" /></p>
+        <p className="text-xl text-center transition-all duration-500 ease-in-out transform opacity-100 hover:opacity-50"></p>
       </div>
     </div>
   );
